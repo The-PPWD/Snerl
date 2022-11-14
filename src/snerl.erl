5cde832e21cd278766d616b37caa69aeb9d6b4ad
Patrick Hofmann <patrickhofmanndesigns@gmail.com>
2022-11-13 18:11:17 -0700

    Got cecho working

--- src/snerl.erl
+++ src/snerl.erl
@@ -1,7 +1,35 @@
 -module(snerl).
 
--export([main/0]).
+-export([main/1]).
 
-main() ->
-    io:format("Snerl"),
+-include_lib("cecho/include/cecho.hrl").
+
+%% exported
+%% main/1
+main(_Args) ->
+    cecho_setup(),
+    game_loop(),
+    cecho_teardown(),
     erlang:halt(0).
+
+
+%% internal
+%% game_loop/0
+game_loop() ->
+    cecho:mvaddstr(0, 0, "Hello, World!"),
+    Key = cecho:getch().
+
+
+%% cecho_setup/0
+cecho_setup() ->
+    application:start(cecho),
+    cecho:cbreak(),
+    cecho:noecho().
+
+
+%% cecho_teardown/0
+cecho_teardown() ->
+    cecho:nocbreak(),
+    cecho:echo(),
+    application:stop(cecho).
+
